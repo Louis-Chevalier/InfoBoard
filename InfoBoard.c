cfd3c3096a04015e569e3caa62b8026367f87d28
Lemiwinx <louis.chevalier@outlook.com>
2021-08-18 17:47:50 -0400

    Fixed bugs in C program and cleared txt file

--- InfoBoard.c
+++ InfoBoard.c
@@ -12,34 +12,41 @@ typedef struct {
 }employeeComp;
 
 void CreateNewFile();//Create and format the new file
-void GetInfo(char pre[Fname], char nom[Lname], int num, int record);
+void GetInfo(char pre[Fname], char nom[Lname], int* num, int* record);
 void PrintInfo(employeeComp Var1);
 
 int main(){
     employeeComp Emp1;
     Emp1.score =0;
     Emp1.streak =0;
-    GetInfo(Emp1.firstname, Emp1.lastname, Emp1.score , Emp1.streak);
-    printf("2)Employee: %s %s, Score: %d, Streak(days): %d\n", Emp1.firstname, Emp1.lastname, Emp1.score, Emp1.streak);
-    //PrintInfo(Emp1);
+    GetInfo(Emp1.firstname, Emp1.lastname, &Emp1.score , &Emp1.streak);
+
+    //For debugging purposes
+    //printf("2)Employee: %s %s, Score: %d, Streak(days): %d\n", Emp1.firstname, Emp1.lastname, Emp1.score, Emp1.streak);
+
+    PrintInfo(Emp1);
     return 0;
 }
 
-void GetInfo(char pre[Fname], char nom[Lname], int num, int record){
+void GetInfo(char pre[Fname], char nom[Lname], int *num, int *record){
+    int num2,record2;
     printf("Please enter the first name: ");
     fgets(pre, Fname, stdin);
     printf("Please enter the last name: ");
     fgets(nom, Lname, stdin);
     printf("Please enter the score: ");
-    scanf("%d", &num);
+    scanf("%d", &num2);
     printf("Please enter any streak held (in days): ");
-    scanf("%d", &record);
+    scanf("%d", &record2);
+    *num = num2;
+    *record = record2;
 
     //Replace "\n" with "\0" at the end of each string
     pre[strcspn(pre,"\n")] = 0;
     nom[strcspn(nom,"\n")] = 0;
 
-    printf("1)Employee: %s %s, Score: %d, Streak(days): %d\n", pre, nom, num, record);
+    //For debugging purposes
+    //printf("1)Employee: %s %s, Score: %d, Streak(days): %d\n", pre, nom, num, record);
 }
 
 void PrintInfo(employeeComp Var2){
